0c9074fff1efc3d2d8fe1445c813867bef5c3f30
Dev <dev.12092003@gmail.com>
2023-10-05 07:13:16 +0530

    commit

--- app/src/main/java/com/example/mad8_63_5b6/MainActivity.kt
+++ app/src/main/java/com/example/mad8_63_5b6/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompact..setDecorFitsSirstSystemWindow(window,false)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
